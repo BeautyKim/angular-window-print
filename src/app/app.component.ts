2becde68724857937f9b61deacad19afea1c1a3a
BeautyKim <89237300+BeautyKim@users.noreply.github.com>
2023-05-09 00:15:03 +0900

    window.print 2가지 방법

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,32 +6,43 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public prtContent;
+  public printContent;
   public initBody;
 
   constructor() {}
-
-  public printMe(): void {
-    this.prtContent = document.getElementById('print');
+  // 새로고침 필요
+  public printMe1(): void {
+    this.printContent = document.getElementById('print1');
     window.print();
+  }
+  // 새로고침 필요x
+  public printMe2(): void {
+    const option = '';
+    const popupPrintContent = document.getElementById('print2').innerHTML;
+    const popupPrint = window.open('', '_blank', 'width=722,height=480');
+    popupPrint.document.write(`
+      <html>
+        <head>
+          <title>popup print</title>
+        </head>
+        <body onload="window.print();">
+          ${popupPrintContent}
+        </body>
+      </html>
+    `);
+    popupPrint.document.close();
   }
 
   @HostListener('window:beforeprint', ['$event'])
   onBeforePrint() {
     console.log('인쇄 전');
     this.initBody = document.body.innerHTML;
-    document.body.innerHTML = this.prtContent.innerHTML;
+    document.body.innerHTML = this.printContent.innerHTML;
   }
   @HostListener('window:afterprint', ['$event'])
   onAfterPrint() {
     console.log('인쇄 후');
     document.body.innerHTML = this.initBody;
     window.location.reload();
-  }
-
-  popup(): void {
-    this.prtContent = document.getElementById('popup');
-    const popupPrint = window.open();
-    window.print();
   }
 }
